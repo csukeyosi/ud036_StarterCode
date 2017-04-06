e01b43ba4291400bf7cb16cea9c3334b93a15743
Carla Sukeyosi <carlasukeyosi@gmail.com>
2017-04-06 11:10:00 -0700

    creates movie.py, entertainment_center.py and gitignore; updates readme.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+How to run?
+-------------
+
+	Run the below command in a terminal.
+		
+		python entertainment_center.py
---
+++ entertainment_center.py
@@ -0,0 +1,19 @@
+import media, fresh_tomatoes
+
+# creation of the movies
+shaun = media.Movie("Shaun of the Dead", 
+					"https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2NjA0NDk0NV5BMl5BanBnXkFtZTcwOTA0OTQzMw@@._V1_SY1000_CR0,0,621,1000_AL_.jpg",
+					"https://www.youtube.com/watch?v=LIfcaZ4pC-4")
+
+othe_guys = media.Movie("The Other Guys",
+					"https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0NDQzNTA2Ml5BMl5BanBnXkFtZTcwNzI2OTQzMw@@._V1_.jpg",
+					"https://www.youtube.com/watch?v=D6WOoUG1eNo")
+
+hot_fuzz = media.Movie("Hot Fuzz",
+						"https://images-na.ssl-images-amazon.com/images/M/MV5BMTIxODg2NDU1MF5BMl5BanBnXkFtZTcwOTc3MDM0MQ@@._V1_.jpg",
+						"https://www.youtube.com/watch?v=ayTnvVpj9t4")
+
+movies = [shaun, othe_guys, hot_fuzz]
+
+# creation of the movies page
+fresh_tomatoes.open_movies_page(movies)
