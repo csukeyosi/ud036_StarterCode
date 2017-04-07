8947ced2ace038f60f0506417851dfae1f6e87cc
Carla Sukeyosi <carlasukeyosi@gmail.com>
2017-04-07 09:00:41 -0700

    fixes merge

--- media.py
+++ media.py
@@ -1,5 +1,4 @@
 class Movie():
-<<<<<<< HEAD
     """Represents a Movie and their properties."""
     def __init__(self, title, poster_image_url, trailer_youtube_url):
         """Creates a new instance of movie with the arguments passed
@@ -7,12 +6,3 @@ class Movie():
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-=======
-	"""Represents a Movie and it properties."""
-	
-	def __init__(self, title, poster_image_url, trailer_youtube_url):
-		self.title = title
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
-		
->>>>>>> f0b4d2b0719c4630871a86bf8278f203ff4591a7
