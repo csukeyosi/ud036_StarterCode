0eefe384b1ce630d998309646cde6bcf51a41512
Carla Sukeyosi <carlasukeyosi@gmail.com>
2017-04-07 08:56:37 -0700

    fixes wrong indentations, wrong spaces and adds comments

--- media.py
+++ media.py
@@ -1,8 +1,8 @@
 class Movie():
-	"""Represents a Movie and their properties."""
-	
-	def __init__(self, title, poster_image_url, trailer_youtube_url):
-		self.title = title
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
-		
+    """Represents a Movie and their properties."""
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        """Creates a new instance of movie with the arguments passed
+        as parameters."""
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
